dd5b44502c4e0425ab10a14c46a7c3b7435cd059
Abdullah Selek <abdullahselek@gmail.com>
2018-03-22 20:49:06 +0000

    Load and read from mat files.

--- hico-multi-classification/hico_processor.py
+++ hico-multi-classification/hico_processor.py
@@ -2,6 +2,9 @@
 
 from __future__ import division
 
+import os
+import scipy.io as sio
+
 class HicoProcessor(object):
     """A processor which reads from mat file and
     prepare output labels.
@@ -18,3 +21,14 @@ class HicoProcessor(object):
 
         self._data_dir = data_dir
         self._anno_mat = anno_mat
+
+    def __load_mat(self, data_dir, anno_mat):
+        """Read the labels from annotation mat file and return.
+        Args:
+          data_dir (str):
+            Directory path for hico data.
+          anno_mat_dir (str):
+            File name for annotation mat file.
+        """
+        path = os.path.join(data_dir, anno_mat)
+        return sio.loadmat(path)
