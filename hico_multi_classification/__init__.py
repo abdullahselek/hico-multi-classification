9e86e13c0df02d9b04e93b521d1334b31e08972c
Abdullah Selek <abdullahselek@gmail.com>
2018-03-23 18:31:37 +0000

    Better naming for main folder and add init.

---
+++ hico_multi_classification/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+'''A Benchmark for Recognizing Human-Object Interactions in Images with Pyhton.'''
+
+from __future__ import absolute_import
+
+__author__       = 'Abdullah Selek'
+__email__        = 'abdullahselek@gmail.com'
+__copyright__    = 'Copyright (c) 2018 Abdullah Selek'
+__license__      = 'MIT License'
+__version__      = '0.1'
+__url__          = 'https://github.com/abdullahselek/hico-multi-classification'
+__download_url__ = 'https://github.com/abdullahselek/hico-multi-classification'
+__description__  = 'A Benchmark for Recognizing Human-Object Interactions in Images with Pyhton.'
+
+from hico_multi_classification import (
+    hico_processor
+)
