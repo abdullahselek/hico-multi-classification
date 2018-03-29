97897d43f5f2b019e818cef39da32b18ba441ec0
Abdullah Selek <abdullahselek@gmail.com>
2018-03-29 19:28:31 +0100

    Inserting int64 features into proto.

--- hico_multi_classification/tfrecord_converter.py
+++ hico_multi_classification/tfrecord_converter.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 
+from __future__ import division
+import tensorflow as tf
+
 class TFRecordConverter(object):
-    """Class converts HICO data to Tensorflow records.
-    """
+    """Class converts HICO data to Tensorflow records."""
     
-    def __init__(self):
-        
+    def __int64_feature(self, value):
+        """Inserting int64 features into proto."""
+
+        if not isinstance(value, list):
+            value = [value]
+        return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
