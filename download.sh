b00bf72e42643ad8e007d57e547b68f10740be09
Abdullah Selek <abdullahselek@gmail.com>
2018-04-05 21:23:22 +0100

    Update download scripts.

--- download.sh
+++ download.sh
@@ -3,9 +3,8 @@
 if [[ "$1" = "hico_data" ]]; then
     mkdir "$1"
     echo "Downlading from http://napoli18.eecs.umich.edu/public_html/data/hico_20150920.tar.gz"
-    curl http://napoli18.eecs.umich.edu/public_html/data/hico_20150920.tar.gz --output "$1"/hico_20150920.tar.gz
-    cd "$1"
-    tar -zxvf hico_20150920.tar.gz
+    curl http://napoli18.eecs.umich.edu/public_html/data/hico_20150920.tar.gz --output hico_20150920.tar.gz
+    tar -zxvf hico_20150920.tar.gz --directory "$1"
     rm hico_20150920.tar.gz
     echo "Hico data downloaded"
     exit 1
@@ -14,11 +13,9 @@ fi
 if [[ "$1" = "inception" ]]; then
     mkdir "$1"
     echo "Downlading from http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz"
-    curl http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz --output "$1"/inception_v3_2016_08_28.tar.gz
-    cd "$1"
-    tar -zxvf inception_v3_2016_08_28.tar.gz
+    curl http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz --output inception_v3_2016_08_28.tar.gz
+    tar -zxvf inception_v3_2016_08_28.tar.gz --directory "$1"
     rm inception_v3_2016_08_28.tar.gz
     echo "Inception downladed"
     exit 1
 fi
-
